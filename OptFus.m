9acca5ec855bd004a1233a6beef7a14a413bea14
Behnood Rasti <61419984+BehnoodRasti@users.noreply.github.com>
2022-03-11 12:02:49 +0100

    Update OptFus.m

--- OptFus.m
+++ OptFus.m
@@ -46,3 +46,20 @@ for i=1:nitr
     fprintf('%d\n',i)
 end
 Fus=reshape(F,n1,m1,r);
+%%
+function normalizedM  = HSInormaliz( X )
+[~, ~, numBands] = size(X);
+normalizedM=X;
+for i=1:numBands
+    normalizedM(:,:,i)  = Normalization( X(:,:,i) );
+end
+end
+%%
+function X_N=Normalization(X)
+[nx1, ny1] = size(X);
+RH=reshape(X,nx1*ny1,1);
+m = min(X(:));
+M = max(X(:));
+NRH=(RH-m)/(M-m);
+X_N=reshape(NRH,nx1,ny1,1);
+end
